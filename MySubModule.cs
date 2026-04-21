d546cbc8769526a1d430ff5eb0b87f1845605304
pedro-ponteiro <silv9797@gmail.com>
2026-04-21 05:24:26 -0300

    initial commit: mercenaries under kingdom service can create armies.

---
+++ MySubModule.cs
@@ -0,0 +1,20 @@
+﻿using ArmyCommander;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace ArmyCommander
+{
+    public class MySubModule : MBSubModuleBase
+    {
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            if (game.GameType is Campaign)
+            {
+                CampaignGameStarter campaignGameStarter = (CampaignGameStarter)gameStarterObject;
+                campaignGameStarter.AddBehavior(new ArmyCommander());
+            }
+        }
+    }
+}
