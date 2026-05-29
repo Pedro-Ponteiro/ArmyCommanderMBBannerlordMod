e144f903bfa0d6d5f05ad797c8ac3cea96abcbb8
pedro-ponteiro <silv9797@gmail.com>
2026-05-29 00:34:57 -0300

    refactor: rename BehaviorStore to Store and ArmyCommands(Behavior)Store

--- Store/ArmyCommandsBehaviorStore.cs
+++ Store/ArmyCommandsBehaviorStore.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Settlements;
 
-namespace ArmyCommander.BehaviorStore
+namespace ArmyCommander.Store
 {
-    public static class ArmyCommandsStore
+    public static class ArmyCommandsBehaviorStore
     {
 
         public static Dictionary<Army, (Army.ArmyTypes ArmyType, Settlement Settlement)> army_commands = new Dictionary<Army, (Army.ArmyTypes ArmyType, Settlement Settlement)>();
