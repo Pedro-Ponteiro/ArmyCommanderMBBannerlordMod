0dbeb613447eb0e4da05eac2288a5fbb5f527026
pedro-ponteiro <silv9797@gmail.com>
2026-06-13 02:44:53 -0300

    feat: add ArmyCommandsContext with ArmyLastVisitedSettlementCache and ArmyIsResupplyingDic.

---
+++ ACBehaviors/Context/ArmyCommandsContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Settlements;
+
+namespace ArmyCommander.ACBehaviors.Context
+{
+    public static class ArmyCommandsContext
+    {
+        public static Dictionary<Army, Settlement> ArmyLastVisitedSettlementCache = new Dictionary<Army, Settlement>();
+
+        public static Dictionary<Army, (bool LookingForFood, bool LookingForTroops)> ArmyIsResupplyingDic = new Dictionary<Army, (bool LookingForFood, bool LookingForTroops)>();
+
+
+        public static void Reset()
+        {
+            ArmyLastVisitedSettlementCache = new Dictionary<Army, Settlement>();
+            ArmyIsResupplyingDic = new Dictionary<Army, (bool LookingForFood, bool LookingForTroops)>();
+        }
+
+    }
+}
