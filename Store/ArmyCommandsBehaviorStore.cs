5a4601f13c8c5f6193fcd71ebc0f7f7087046fc8
pedro-ponteiro <silv9797@gmail.com>
2026-06-13 02:22:49 -0300

    feat: changed some other things

--- Store/ArmyCommandsBehaviorStore.cs
+++ Store/ArmyCommandsBehaviorStore.cs
@@ -11,8 +11,37 @@ namespace ArmyCommander.Store
     public static class ArmyCommandsBehaviorStore
     {
 
-        public static Dictionary<Army, (Army.ArmyTypes ArmyType, Settlement Settlement)> army_commands = new Dictionary<Army, (Army.ArmyTypes ArmyType, Settlement Settlement)>();
+        public static Dictionary<Army, (Army.ArmyTypes ArmyType,
+            Settlement TargetSettlement,
+            Settlement GatherSettlement,
+            bool CanEngageEnemyParties,
+            bool CanHelpAlliedParties,
+            bool CanResupply,
+            bool CanRunFromDanger
+            )> army_commands = 
+            new Dictionary<Army, (Army.ArmyTypes ArmyType, 
+                Settlement TargetSettlement, 
+                Settlement GatherSettlement, 
+                bool CanEngageEnemyParties,
+                bool CanHelpAlliedParties,
+                bool CanResupply,
+                bool CanRunFromDanger
+                )>();
 
+
+
+        public static void Reset()
+        {
+            army_commands =
+            new Dictionary<Army, (Army.ArmyTypes ArmyType,
+                Settlement TargetSettlement,
+                Settlement GatherSettlement,
+                bool CanEngageEnemyParties,
+                bool CanHelpAlliedParties,
+                bool CanResupply,
+                bool CanRunFromDanger
+                )>();
+        }
 
     }
 }
